792163a314789980d71141a0aa53fc0f99864b63
JuanLicciardi <juanelicciardi@gmail.com>
2022-11-25 10:49:54 -0300

    Iniciando TP Interactuando con formularios

---
+++ public/js/moviesAddValidation.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    console.log("estamos llegando")
+
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+//------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
+//-------------------DE REGISTRO DE PELÍCULAS------------------//    
+
+
+
+
+}
